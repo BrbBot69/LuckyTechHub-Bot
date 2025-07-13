625106c443dcb65cb9be78710a091f76f3b00696
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-07-13 16:57:47 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Lucky Tech Hub Bot',
-  author: '‎',
-  botName: "Lucky Tech Hub Bot",
-  botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  packname: '☚⍢⃝☚',
+  author: '⟣‎',
+  botName: "𝕬𝖛𝖊.𝕭☚⍢⃝☚",
+  botOwner: '⟣𝕬𝖛𝖊.𝕭', // Your name
+  ownerNumber: '27767494368', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
